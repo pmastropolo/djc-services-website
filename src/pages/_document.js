a0e55ca563d18b90f9c0dc5e328429882b1601a3
Peyton Mastropolo <amyhytwn@aol.com>
2024-05-15 08:54:29 -0500

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,13 +6,11 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* meta begin */}
+          {/* Meta Tags */}
           <meta charSet="UTF-8" />
           <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-          
-          {/* meta end */}
 
-          {/* favicon begin */}
+          {/* Favicon */}
           <link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
@@ -20,13 +18,11 @@ class MyDocument extends Document {
           <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
-          {/* favicon */}
 
-          {/* public assets begin */}
+          {/* Public Assets */}
           <link rel="stylesheet" href="/css/bootstrap.min.css" />
           <link rel="stylesheet" href="/css/fontawesome.min.css" />
           <link rel="stylesheet" href="/css/magnific-popup.css" />
-          {/* public assets end */}
 
           {/* Google Analytics */}
           <script async src="https://www.googletagmanager.com/gtag/js?id=G-CCHMX6MXWM"></script>
@@ -43,13 +39,11 @@ class MyDocument extends Document {
             }}
           />
 
+          {/* Mailchimp */}
           <Script 
-  src="https://chimpstatic.com/mcjs-connected/js/users/efcfec60b837c71d5e756a138/96beb85639cbd8c95d34d9c80.js"
-  strategy="afterInteractive" 
-/>
-
-
-
+            src="https://chimpstatic.com/mcjs-connected/js/users/efcfec60b837c71d5e756a138/96beb85639cbd8c95d34d9c80.js"
+            strategy="afterInteractive" 
+          />
         </Head>
         <body>
           <Main />
@@ -61,3 +55,4 @@ class MyDocument extends Document {
 }
 
 export default MyDocument;
+
