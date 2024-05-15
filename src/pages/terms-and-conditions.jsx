7d45466617f57b9838c2c75024edc94820cfcce2
Peyton Mastropolo <amyhytwn@aol.com>
2024-05-15 11:35:38 -0500

    Update terms-and-conditions.jsx

--- src/pages/terms-and-conditions.jsx
+++ src/pages/terms-and-conditions.jsx
@@ -73,6 +73,35 @@ const TermsConditions = () => {
 <p>The Site may contain bulletin board services, chat areas, news groups, forums, communities, personal web pages, calendars, and/or other message or communication facilities designed to enable you to communicate with the public at large or with a group (collectively, "Communication Services"). You agree to use the Communication Services only to post, send and receive messages and material that are proper and related to the particular Communication Service.</p>
 <p>By way of example, and not as a limitation, you agree that when using a Communication Service, you will not: defame, abuse, harass, stalk, threaten or otherwise violate the legal rights (such as rights of privacy and publicity) of others; publish, post, upload, distribute or disseminate any inappropriate, profane, defamatory, infringing, obscene, indecent or unlawful topic, name, material or information; upload files that contain software or other material protected by intellectual property laws (or by rights of privacy of publicity) unless you own or control the rights thereto or have received all necessary consents; upload files that contain viruses, corrupted files, or any other similar software or programs that may damage the operation of another’s computer; advertise or offer to sell or buy any goods or services for any business purpose, unless such Communication Service specifically allows such messages; conduct or forward surveys, contests, pyramid schemes or chain letters; download any file posted by another user of a Communication Service that you know, or reasonably should know, cannot be legally distributed in such manner; falsify or delete any author attributions, legal or other proper notices or proprietary designations or labels of the origin or source of software or other material contained in a file that is uploaded, restrict or inhibit any other user from using and enjoying the Communication Services; violate any code of conduct or other guidelines which may be applicable for any particular Communication Service; harvest or otherwise collect information about others, including e-mail addresses, without their consent; violate any applicable laws or regulations.</p>
 
+<h2 style={styles.h2}>Monitoring and Disclosure</h2>
+<p>
+  DJCServices, LLC has no obligation to monitor the Communication Services. However, DJCServices, LLC reserves the right to review materials posted to a Communication Service and to remove any materials in its sole discretion. DJCServices, LLC reserves the right to terminate your access to any or all of the Communication Services at any time without notice for any reason whatsoever.
+</p>
+<p>
+  DJCServices, LLC reserves the right at all times to disclose any information as necessary to satisfy any applicable law, regulation, legal process or governmental request, or to edit, refuse to post or to remove any information or materials, in whole or in part, in DJCServices, LLC's sole discretion.
+</p>
+<p>
+  Always use caution when giving out any personally identifying information about yourself or your children in any Communication Service. DJCServices, LLC does not control or endorse the content, messages or information found in any Communication Service and, therefore, DJCServices, LLC specifically disclaims any liability with regard to the Communication Services and any actions resulting from your participation in any Communication Service. Managers and hosts are not authorized DJCServices, LLC spokespersons, and their views do not necessarily reflect those of DJCServices, LLC.
+</p>
+<p>
+  Materials uploaded to a Communication Service may be subject to posted limitations on usage, reproduction and/or dissemination. You are responsible for adhering to such limitations if you upload the materials.
+</p>
+
+<h2 style={styles.h2}>Materials Provided to <a href="https://djcservicesllc.com/">https://djcservicesllc.com/</a> or Posted on Any DJCServices, LLC Web Page</h2>
+<p>
+  DJCServices, LLC does not claim ownership of the materials you provide to <a href="https://djcservicesllc.com/">https://djcservicesllc.com/</a> (including feedback and suggestions) or post, upload, input or submit to any DJCServices, LLC Site or our associated services (collectively "Submissions"). However, by posting, uploading, inputting, providing or submitting your Submission you are granting DJCServices, LLC, our affiliated companies and necessary sublicensees permission to use your Submission in connection with the operation of their Internet businesses including, without limitation, the rights to: copy, distribute, transmit, publicly display, publicly perform, reproduce, edit, translate and reformat your Submission; and to publish your name in connection with your Submission.
+</p>
+<p>
+  No compensation will be paid with respect to the use of your Submission, as provided herein. DJCServices, LLC is under no obligation to post or use any Submission you may provide and may remove any Submission at any time in DJCServices, LLC's sole discretion.
+</p>
+<p>
+  By posting, uploading, inputting, providing or submitting your Submission you warrant and represent that you own or otherwise control all of the rights to your Submission as described in this section including, without limitation, all the rights necessary for you to provide, post, upload, input or submit the Submissions.
+</p>
+
+<h2 style={styles.h2}>Third Party Accounts</h2>
+<p>
+  You will be able to connect your DJCServices, LLC account to third party accounts. By connecting your DJCServices, LLC account to your third party account, you acknowledge and agree that you are consenting to the continuous release of information about you to others (in accordance with your privacy settings on those third party sites). If you do not want information about you to be shared in this manner, do not use this feature.
+</p>
 
 
 
