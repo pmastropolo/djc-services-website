ce2f7cd08b5785df9ca5d263d248bdd2faca24b0
Peyton Mastropolo <amyhytwn@aol.com>
2024-05-15 08:55:29 -0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -15,13 +15,12 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-          {/* seo begin */}
-          <title>{appData.settings.siteName}</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          {/* seo end */}        
+        {/* SEO */}
+        <title>{appData.settings.siteName}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <AuthContextProvider>
-      <Component {...pageProps} />
+        <Component {...pageProps} />
       </AuthContextProvider>
     </>
   );
