56cca1b53b61ecd537c61ee31314bb0422e092a0
Peyton Mastropolo <amyhytwn@aol.com>
2024-05-15 11:28:55 -0500

    Update terms-and-conditions.jsx

--- src/pages/terms-and-conditions.jsx
+++ src/pages/terms-and-conditions.jsx
@@ -38,6 +38,47 @@ const TermsConditions = () => {
 
       <div style={styles.container}> 
         <h1 style={styles.h1}>Terms and Conditions</h1>
+        
+        <h2 style={styles.h2}>Agreement between User and <a href="https://djcservicesllc.com/">https://djcservicesllc.com/</a></h2>
+        <p>Welcome to <a href="https://djcservicesllc.com/">https://djcservicesllc.com/</a>. The <a href="https://djcservicesllc.com/">https://djcservicesllc.com/</a> website (the "Site") is comprised of various web pages operated by DJCServices, LLC. <a href="https://djcservicesllc.com/">https://djcservicesllc.com/</a> is offered to you conditioned on your acceptance without modification of the terms, conditions, and notices contained herein (the "Terms"). Your use of <a href="https://djcservicesllc.com/">https://djcservicesllc.com/</a> constitutes your agreement to all such Terms. Please read these terms carefully, and keep a copy of them for your reference.</p>
+        <p><a href="https://djcservicesllc.com/">https://djcservicesllc.com/</a> is an E-Commerce Site.</p>
+        <p>Welcome to DJCServices, where we specialize in crafting your dream spaces. From innovative designs to meticulous construction, we're dedicated to exceeding your expectations.</p>
+
+<h2 style={styles.h2}>Privacy</h2>
+<p>Your use of <a href="https://djcservicesllc.com/">https://djcservicesllc.com/</a> is subject to DJCServices, LLC's Privacy Policy. Please review our Privacy Policy, which also governs the Site and informs users of our data collection practices.</p>
+
+<h2 style={styles.h2}>Electronic Communications</h2>
+<p>Visiting <a href="https://djcservicesllc.com/">https://djcservicesllc.com/</a> or sending emails to DJCServices, LLC constitutes electronic communications. You consent to receive electronic communications and you agree that all agreements, notices, disclosures, and other communications that we provide to you electronically, via email and on the Site, satisfy any legal requirement that such communications be in writing.
+</p>
+
+<h2 style={styles.h2}>Your Account</h2>
+<p>If you use this site, you are responsible for maintaining the confidentiality of your account and password and for restricting access to your computer, and you agree to accept responsibility for all activities that occur under your account or password. You may not assign or otherwise transfer your account to any other person or entity. You acknowledge that DJCServices, LLC is not responsible for third-party access to your account that results from theft or misappropriation of your account. DJCServices, LLC and its associates reserve the right to refuse or cancel service, terminate accounts, or remove or edit content in our sole discretion.</p>
+
+<h2 style={styles.h2}>Children Under Thirteen</h2>
+<p>
+  DJCServices, LLC does not knowingly collect, either online or offline, personal information from persons under the age of thirteen. If you are under 18, you may use <a href="https://djcservicesllc.com/">https://djcservicesllc.com/</a> only with permission of a parent or guardian.
+</p>
+
+
+<h2 style={styles.h2}>Links to Third Party Sites/Third Party Services</h2>
+<p><a href="https://djcservicesllc.com/">https://djcservicesllc.com/</a> may contain links to other websites ("Linked Sites"). The Linked Sites are not under the control of DJCServices, LLC and DJCServices, LLC is not responsible for the contents of any Linked Site, including without limitation any link contained in a Linked Site, or any changes or updates to a Linked Site. DJCServices, LLC is providing these links to you only as a convenience, and the inclusion of any link does not imply endorsement by DJCServices, LLC of the site or any association with its operators.</p>
+<p>Certain services made available via <a href="https://djcservicesllc.com/">https://djcservicesllc.com/</a> are delivered by third party sites and organizations. By using any product, service or functionality originating from the <a href="https://djcservicesllc.com/">https://djcservicesllc.com/</a> domain, you hereby acknowledge and consent that DJCServices, LLC may share such information and data with any third party with whom DJCServices, LLC has a contractual relationship to provide the requested product, service or functionality on behalf of <a href="https://djcservicesllc.com/">https://djcservicesllc.com/</a> users and customers.</p>
+
+<h2 style={styles.h2}>No Unlawful or Prohibited Use/Intellectual Property</h2>
+<p>You are granted a non-exclusive, non-transferable, revocable license to access and use <a href="https://djcservicesllc.com/">https://djcservicesllc.com/</a> strictly in accordance with these terms of use. As a condition of your use of the Site, you warrant to DJCServices, LLC that you will not use the Site for any purpose that is unlawful or prohibited by these Terms. You may not use the Site in any manner which could damage, disable, overburden, or impair the Site or interfere with any other party’s use and enjoyment of the Site. You may not obtain or attempt to obtain any materials or information through any means not intentionally made available or provided for through the Site.</p>
+<p>All content included as part of the Service, such as text, graphics, logos, images, as well as the compilation thereof, and any software used on the Site, is the property of DJCServices, LLC or its suppliers and protected by copyright and other laws that protect intellectual property and proprietary rights. You agree to observe and abide by all copyright and other proprietary notices, legends or other restrictions contained in any such content and will not make any changes thereto.</p>
+<p>You will not modify, publish, transmit, reverse engineer, participate in the transfer or sale, create derivative works, or in any way exploit any of the content, in whole or in part, found on the Site. DJCServices, LLC content is not for resale. Your use of the Site does not entitle you to make any unauthorized use of any protected content, and in particular you will not delete or alter any proprietary rights or attribution notices in any content. You will use protected content solely for your personal use, and will make no other use of the content without the express written permission of DJCServices, LLC and the copyright owner. You agree that you do not acquire any ownership rights in any protected content. We do not grant you any licenses, express or implied, to the intellectual property of DJCServices, LLC or our licensors except as expressly authorized by these Terms.</p>
+
+<h2 style={styles.h2}>Use of Communication Services</h2>
+<p>The Site may contain bulletin board services, chat areas, news groups, forums, communities, personal web pages, calendars, and/or other message or communication facilities designed to enable you to communicate with the public at large or with a group (collectively, "Communication Services"). You agree to use the Communication Services only to post, send and receive messages and material that are proper and related to the particular Communication Service.</p>
+<p>By way of example, and not as a limitation, you agree that when using a Communication Service, you will not: defame, abuse, harass, stalk, threaten or otherwise violate the legal rights (such as rights of privacy and publicity) of others; publish, post, upload, distribute or disseminate any inappropriate, profane, defamatory, infringing, obscene, indecent or unlawful topic, name, material or information; upload files that contain software or other material protected by intellectual property laws (or by rights of privacy of publicity) unless you own or control the rights thereto or have received all necessary consents; upload files that contain viruses, corrupted files, or any other similar software or programs that may damage the operation of another’s computer; advertise or offer to sell or buy any goods or services for any business purpose, unless such Communication Service specifically allows such messages; conduct or forward surveys, contests, pyramid schemes or chain letters; download any file posted by another user of a Communication Service that you know, or reasonably should know, cannot be legally distributed in such manner; falsify or delete any author attributions, legal or other proper notices or proprietary designations or labels of the origin or source of software or other material contained in a file that is uploaded, restrict or inhibit any other user from using and enjoying the Communication Services; violate any code of conduct or other guidelines which may be applicable for any particular Communication Service; harvest or otherwise collect information about others, including e-mail addresses, without their consent; violate any applicable laws or regulations.</p>
+
+
+
+
+
+
+        
 
         <p>Welcome to DJCServices, LLC! We are located in Helena, AL and provide premier construction services. By accessing and using our services, you agree to comply with and be bound by the following terms and conditions. Please review these terms carefully.</p>
 
